7f55083c21c9b6da94bec39a7bec955587a84544
kayvank <kayvan@q2io.com>
2018-07-06 06:49:31 -0700

    fix - add readme docs

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# arabic-numerals
+# RHO Arabic numeral assignment
+Arabic Numeral Homework and notes form [Greg Meredith](https://www.youtube.com/watch?v=tY5AEuz6lCU&t=34s) lectures.
+
+## Getting Started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system
+
+### Prerequisites
+
+#### Installs
+- [stack](https://docs.haskellstack.org/en/stable/README)
+
+#### clone the repo
+```
+git clone git@github.com:kayvank/arabic-numerals.git
+```
+
+## Ruunig the tests
+```
+cd arabic-numerals
+stack test
+```
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,12 +5,19 @@ import Data.Monoid
 main :: IO ()
 main = hspec $ do
  describe "Arabic Numerals specs" $ do
+
    it "must correctly represent construct 0" $ 
       value Zero == 0 `shouldBe` True
+
    it "must correctly construct 3" $ 
       value (Inc(Inc (Inc Zero))) == 3 `shouldBe` True
+
    it "must correctly add 2 and 3 using monoid mappend" $ 
      value (Inc(Inc(Zero)) `mappend` Inc(Inc(Inc(Zero))))  == 5 `shouldBe` True
+
+   it "must correctly represent ordering" $ 
+     Inc(Inc(Zero)) < Inc(Inc(Inc(Zero)))  `shouldBe` True
+
    it "must observe addition  commutative property" $ 
      value (Inc(Inc(Zero)) `mappend` Inc(Inc(Inc(Zero))))  ==
         value (Inc(Inc(Zero)) `mappend` Inc(Inc(Inc(Zero))))  `shouldBe` True
